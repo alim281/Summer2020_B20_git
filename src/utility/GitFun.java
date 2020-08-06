27c64d30d22e11ecc0e0ee2de730a59175a3c9f2
alim281 <68941969+alim281@users.noreply.github.com>
2020-08-05 18:32:47 -0700

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,6 +3,8 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Everthing is fun");
+        
+        // This line added from GitHub
 
 
 
