40402624634dc955793398cf991a64b375887e1c
alim281 <68941969+alim281@users.noreply.github.com>
2020-08-05 18:39:00 -0700

    Update MissJava.java

--- src/utility/MissJava.java
+++ src/utility/MissJava.java
@@ -2,7 +2,7 @@ package utility;
 
 public class MissJava {
     public static void main(String[] args) {
-        System.out.println("Miss Java");
+      
       
         // print order deleted
     }
